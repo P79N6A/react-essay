db50591b5ae67110c26677c417e658ad4236234d
jianfehuang(黄剑枫) <jianfehuang@tencent.com>
2018-12-25 20:16:36 +0800

    new

---
+++ redux-02/src/AddTodo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class AddTodo extends Component {
+ render() {
+  return (
+   <div>
+       <input type="text" />
+       <button type="button">Add</button>
+   </div>
+  )
+ }
+}
+
+export default AddTodo
--- redux-02/src/app.js
+++ redux-02/src/app.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+import Footer from './Footer'
+
+const data = [
+    {
+        id: '1',
+        text: '001',
+        completed: false,
+    },
+    {
+        id: '2',
+        text: '002',
+        completed: true,
+    },
+]
 
 class App extends Component {
+
+    constructor (props) {
+        super(props)
+        this.onTodoClick = this.onTodoClick.bind(this)
+    }
+
+    onTodoClick (id) {
+        console.log('@@@@todo click', id)
+    }
+
     render() {
         return (
-            <div>{'react app'}</div>
+            <div>
+                <AddTodo />
+                <TodoList todos={data} onTodoClick={this.onTodoClick}/>
+                <Footer />
+            </div>
         )
     }
 }
