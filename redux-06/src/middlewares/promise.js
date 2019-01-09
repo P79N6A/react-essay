26b8c84663c0cfa464f398c13e2d6000e2891bad
suifengfengye <1527261080@qq.com>
2019-01-10 02:17:29 +0800

    new

--- redux-06/src/middlewares/promise.js
+++ redux-06/src/middlewares/promise.js
@@ -1,16 +1,21 @@
 
 const promise = (store) => (next) => (action) => {
+    // 抽取一个dispatch的方法
+    // 支持派发多个actions
+    const dispatchInnerAction = (data) => {
+        if (Array.isArray(data)) {
+            data.forEach(item => {
+                store.dispatch(item)
+            })
+        } else {
+            store.dispatch(data)
+        }
+    }
     if (typeof action.then === 'function') {
         action.then((data) => {
-            if (Array.isArray(data)) {
-                data.forEach(item => {
-                    store.dispatch(item)
-                })
-            } else {
-                store.dispatch(data)
-            }
+            dispatchInnerAction(data)
         }, (err) => {
-            store.dispatch(err)
+            dispatchInnerAction(err)
         })
     } else {
         next(action)
