82ae60f9752c01f04779c71968b87445cd0bc84c
suifengfengye <1527261080@qq.com>
2019-03-30 15:09:26 +0800

    complete ace editor

--- react-ace-00/src/app.js
+++ react-ace-00/src/app.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace'
+
+let editorRef = null
+
 class App extends Component {
+
+    handleChange = () => {
+        this.update()
+    }
+
+    update = () => {
+        if (!editorRef) {
+            return
+        }
+        const editor = editorRef.editor
+        let shouldShow = !editor.session.getValue().length
+        let node = editor.renderer.emptyMessageNode
+        if (!shouldShow && node) {
+            editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode)
+            editor.renderer.emptyMessageNode = null
+        } else if (shouldShow && !node) {
+            node = document.createElement('div')
+            editor.renderer.emptyMessageNode = node
+            node.innerHTML = '我是placeholder~~~~'
+            node.className = 'ace_invisible ace_emptyMessage'
+            node.style.padding = '0 9px'
+            node.style.position = 'absolute'
+            node.style.zIndex = 5
+            editor.renderer.scroller.appendChild(node)
+        }
+    }
+
     render() {
+        const { content } = this.state
         return (
             <div>
-                <AceEditor 
+                <AceEditor
+                    onChange={this.handleChange}
+                    ref={(r) => {
+                        editorRef = r
+                        setTimeout(() => {
+                            this.update()
+                        })
+                    }}
                     placeholder="placeholder ace editor"
                 />
             </div>
